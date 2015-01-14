9ffdad1c151a1816d4ca1955e6b4d970c431ad5b
Mikael Couzic <mikaelcouzic@gmail.com>
2015-01-14 17:41:11 +0100

    gulp tdd

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,27 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
-    nodemon = require('gulp-nodemon');
+    nodemon = require('gulp-nodemon'),
+    mocha = require('gulp-mocha-co'),
+    env = require('gulp-env');
 
 var pathToSrc = ['config/**/*.js', 'src/**/*.js', 'gulpfile.js'];
+var pathToTest = 'test/**/*.spec.js';
 
-gulp.task('default', ['dev','lint'], function () {
+gulp.task('default', ['dev', 'lint'], function () {
 });
 
 gulp.task('dev', function () {
-    nodemon({ script: 'index.js' })
+    nodemon({script: 'index.js'})
         .on('change', ['lint']);
 });
 
 gulp.task('lint', function () {
     gulp.src(pathToSrc)
         .pipe(jshint({
-            strict:false
+            strict: false
         }))
         .pipe(jshint.reporter('jshint-stylish'));
 });
-
 
 
 gulp.task('set-env-test', function () {
@@ -30,7 +32,15 @@ gulp.task('set-env-test', function () {
     });
 });
 
-gulp.task('test',['set-env-test'] ,function () {
-    gulp.src('./tests/*.spec.js')
-        .pipe(mocha({reporter: 'nyan'}));
+gulp.task('test', ['set-env-test'], function () {
+    gulp.src(pathToTest)
+        .pipe(mocha({reporter: 'nyan'}))
+        .on('error', function logMessage(error) {
+            console.log(error.message);
+        });
+});
+
+gulp.task('tdd', ['test'], function () {
+    gulp.watch(pathToSrc, ['test']);
+    gulp.watch(pathToTest, ['test']);
 });
---
+++ test/auth.spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('should');
+
+describe('Test env', function () {
+
+    it('should pass', function () {
+        true.should.be.true;
+        //true.should.be.false;
+    });
+
+});
